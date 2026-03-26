0509ef931d5fd147eb9ed50d14fff81f56214985
mmtol <mmigueltolosana@gmail.com>
2026-03-26 11:51:14 +0100

    aaa

--- MvcApiCrudDepartamentos/wwwroot/js/serviceapidepartamentos.js
+++ MvcApiCrudDepartamentos/wwwroot/js/serviceapidepartamentos.js
@@ -1,20 +1,22 @@
 ﻿let url = "https://apicorecruddepartamentosmmt.azurewebsites.net/";
-function loadDepts() {
+
+function getDeptsAsync(callBack) {
     let request = "api/departamentos";
     $.ajax({
         url: url + request,
         method: "GET",
         success: function (data) {
             console.log("leyendo");
-            var html = "";
-            $.each(data, function (index, departamento) {
-                html += "<tr>";
-                html += "<td>" + departamento.idDepartamento + "</td>";
-                html += "<td>" + departamento.nombre + "</td>";
-                html += "<td>" + departamento.localidad + "</td>";
-                html += "</tr>";
-            })
-            $("#tabladepts tbody").html(html);
+            callBack(data);
+            // var html = "";
+            // $.each(data, function(index, departamento){
+            //     html += "<tr>";
+            //         html += "<td>"+ departamento.idDepartamento +"</td>";
+            //         html += "<td>"+ departamento.nombre +"</td>";
+            //         html += "<td>"+ departamento.localidad +"</td>";
+            //     html += "</tr>";
+            // })
+            // $("#tabladepts tbody").html(html);
         }
     })
 }
